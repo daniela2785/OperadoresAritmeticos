3ec988f4e836dd6c45c5a8704aac0d46311d4910
daniela marques <dd978452@gmail.com>
2023-01-16 09:18:48 -0300

    Adicionar arquivos de projeto.

---
+++ OperadoresAritmeticos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OperadoresAritmeticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroQualquer = 20 + 20;
+            int numeroMult = 10 * 90;
+            float numeroDiv = 5f / 2;
+            
+            int teste = 2 + 20 * numeroMult;
+
+            // 6 / 2 => // int
+            // 5 / 2 => // decimal //float 
+             
+
+
+            Console.WriteLine(numeroQualquer);
+            Console.WriteLine(numeroMult);
+            Console.WriteLine(numeroDiv);   
+            Console.WriteLine(teste); 
+        }
+    }
+}
